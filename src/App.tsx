e9a7bdfe66dcf0128e9d6906c121dd25dd5abaec
Akshat Dhiman <akshat.dhiman15@gmail.com>
2024-12-19 01:37:54 +0530

    update: default dark background added

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import '@mantine/core/styles.css';
 
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
-import { theme } from './theme';
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider defaultColorScheme="dark">
       <Router />
     </MantineProvider>
   );
